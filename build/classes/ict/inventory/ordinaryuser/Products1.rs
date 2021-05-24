11733c6ee1802c34651ec486aba2f4c79c9f6c6e
Danver Kanyemba <danverkanyemba33@gmail.com>
2021-05-24 16:22:16 +0200

    Created Jar file for project

--- build/classes/ict/inventory/ordinaryuser/Products1.rs
+++
@@ -1,1 +0,0 @@
-ict.inventory.ordinaryuser.productsAndClient
--- build/classes/ict/inventory/ordinaryuser/reportsforinventory/NewJFrame.rs
+++
@@ -1,3 +0,0 @@
-ict.inventory.reportsforinventory.NewJFrame$2
-ict.inventory.reportsforinventory.NewJFrame$1
-ict.inventory.reportsforinventory.NewJFrame
